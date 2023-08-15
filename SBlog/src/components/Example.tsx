ea9ab2714ff7d29356f8867490fea59167eedc0d
Zaki Yusaf <zakiyusaf@hotmail.co.uk>
2023-08-15 20:49:17 +0100

    Add client with vite+express server boilerplate

---
+++ SBlog/src/components/Example.tsx
@@ -0,0 +1,16 @@
+
+
+const Example = () => {
+
+    return(
+        <>
+            <ul>
+                <li><a>Home</a></li>
+                <li><a>Posts</a></li>
+                <li><a>About</a></li>
+            </ul>
+        </>
+    )
+}
+
+export default Example;
